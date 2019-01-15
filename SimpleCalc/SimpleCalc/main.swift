f3f9e223879ee2bbef8c7535e2b4cb408f995180
Brian Luu <luubri5@uw.edu>
2019-01-15 13:45:23 -0800

    Tests passed

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,7 +10,46 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        if args.count < 2 {
+            return 0
+        }
+        switch args.last {
+        case "count":
+            return args.count - 1
+        case "avg":
+            var sum = 0
+            for i in 0...args.count - 2 {
+                sum += Int(args[i])!
+            }
+        
+            return sum / (args.count - 1)
+        case "fact":
+            if args[0] == "0" {
+                return 1
+            }
+            var factorialSum = 1
+            for i in 1...Int(args[0])! {
+                factorialSum *= i
+            }
+            return factorialSum
+        default:
+            switch args[1] {
+            case "+":
+                return Int(args[0])! + Int(args[2])!
+            case "-":
+                return Int(args[0])! - Int(args[2])!
+            case "*":
+                return Int(args[0])! * Int(args[2])!
+            case "/":
+                return Int(args[0])! / Int(args[2])!
+            case "%":
+                return Int(args[0])! % Int(args[2])!
+            default:
+                return 0
+            }
+        }
     }
+
     
     public func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
